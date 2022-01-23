e549390fc22ca6a072f78ae3ff45a2d0b84437a9
Maximiliano R. Gomez <48895848+Maximiliano-Gomez@users.noreply.github.com>
2022-01-23 02:00:55 -0300

    firstCommit
    initial

---
+++ script.js
@@ -0,0 +1,49 @@
+const toggleWitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textbox = document.getElementById('text-box');
+
+// Dark Mode Styles
+function toggleDarkLightMode(isDark) {
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    textbox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    isDark ? imageMode('dark') : imageMode('light');
+}
+
+// Dark or Light Images 
+function imageMode(color) {
+    image1.src = `img/setup_${color}.svg`;
+    image2.src = `img/follower_${color}.svg`;
+    image3.src = `img/social_${color}.svg`;
+}
+
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark')
+        toggleDarkLightMode(true);
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        toggleDarkLightMode(false);
+        localStorage.setItem('theme', 'light')
+    }
+}
+
+// Event Listener
+toggleWitch.addEventListener('change', switchTheme);
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLightMode(true);
+    }
+}
